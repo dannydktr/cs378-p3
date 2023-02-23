eae0cbd2c02ae5906e015b0122384c082f16e125
dannydktr <93729067+dannydktr@users.noreply.github.com>
2023-02-23 13:08:10 -0600

    added basic buttons and fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const [buttonNames, setButtonNames] = useState([]);
+
+  const handleButtonClick = (longitude, latitude) => {
+    const newButton = (
+      <button onClick={() => fetchAPIData('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m')}>
+        Longitude: {longitude}
+        Latitude: {latitude}
+      </button>
+    );
+    setButtonNames([...buttonNames, newButton]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label>Longitude</label>
+        <input type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)} ></input>
+      </div>
+      <div>
+        <label>Latitude</label>
+        <input type="text" value={latitude} onChange={(e) => setLatitude(e.target.value)} ></input>
+      </div>
+      <div>
+        <button onClick={() => handleButtonClick(longitude, latitude)}>Add New Location</button>
+      </div>
+      <div>
+        {buttonNames}
+      </div>
     </div>
   );
 }
 
-export default App;
+async function fetchAPIData(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json.hourly);
+  } catch (err) {
+    console.log('Error trying to fetch data');
+  }
+}
